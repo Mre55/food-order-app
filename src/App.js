3199825e91ae35e4853fed46b6f4636545d9fc98
mre55 <misganawmihreteab>
2023-01-30 22:42:56 +0300

    Render Cart component to the page

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Cart from "./components/Cart/Cart";
 
 import Header from "./components/Layout/Header";
 import Meals from "./components/Meals/Meals";
@@ -6,6 +7,7 @@ import Meals from "./components/Meals/Meals";
 function App() {
   return (
     <>
+      <Cart />
       <Header />
       <main>
         <Meals />
