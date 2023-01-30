15cdc94bd2c7ab9bfdb7157d4196a86e6b6ea4f9
mre55 <misganawmihreteab>
2023-01-30 16:51:48 +0300

    Used HeaderCart component inside Header component

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,5 +1,6 @@
 import React from "react";
 
+import HeaderCartButton from "./HeaderCartButton";
 import classes from "./Header.module.css";
 import mealsImage from "../../assets/meals.jpeg";
 
@@ -8,7 +9,7 @@ const Header = () => {
     <>
       <header className={classes.header}>
         <h1>ReactMeals</h1>
-        <button>Cart</button>
+        <HeaderCartButton />
       </header>
       <div className={classes["main-image"]}>
         <img src={mealsImage} alt="A table full of delicious food!" />
