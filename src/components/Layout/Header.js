744c79ebeac694d6e4d91ff98a020e388d26f860
mre55 <misganawmihreteab>
2023-01-30 23:29:53 +0300

    Add Closing modal with close button

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -4,12 +4,12 @@ import HeaderCartButton from "./HeaderCartButton";
 import classes from "./Header.module.css";
 import mealsImage from "../../assets/meals.jpeg";
 
-const Header = () => {
+const Header = (props) => {
   return (
     <>
       <header className={classes.header}>
         <h1>ReactMeals</h1>
-        <HeaderCartButton />
+        <HeaderCartButton onClick={props.onShowCart} />
       </header>
       <div className={classes["main-image"]}>
         <img src={mealsImage} alt="A table full of delicious food!" />
